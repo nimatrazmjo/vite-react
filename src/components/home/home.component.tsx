11d32a467cf350963bc1f0ce6177b48402905015
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2023-01-03 16:40:01 +0500

    Add isax font package

---
+++ src/components/home/home.component.tsx
@@ -0,0 +1,11 @@
+import { HomeHashtag } from 'iconsax-react';
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <div className='items-center justify-center'>
+            <h1>Home</h1>
+            <HomeHashtag size="32" color="#FF8A65" />
+        </div>
+    );
+};
